7a17a6af35074db9d5b317ce714a1930842da54a
ravvi1104 <rry4298@gmail.com>
2023-09-29 07:12:51 +0530

    added remove btn to all fav list items

--- Favorites/favorites.js
+++ Favorites/favorites.js
@@ -15,6 +15,7 @@ function displayFavorites() {
 }
 
 // Function to create a favorite superhero card
+// Function to create a favorite superhero card
 function createFavoriteCard(superhero, favoriteList) {
     const listItem = document.createElement("div");
     listItem.className = "favorite-item";
@@ -26,19 +27,31 @@ function createFavoriteCard(superhero, favoriteList) {
             <div class="card-body">
                 <p class="card-text">${superhero.name}</p>
             </div>
+            <button data-id="${superhero.id}" class="btn btn-outline-danger del-btn" type="button">Remove</button>
         </div>
     `;
+
+    // Attach a click event listener to the "Remove" button
+    const removeButton = listItem.querySelector(".del-btn");
+    removeButton.addEventListener('click', () => {
+        const superheroIdToRemove = removeButton.getAttribute("data-id");
+        removeFavorite(superheroIdToRemove);
+        listItem.remove(); // Remove the card from the display
+    });
 
     favoriteList.appendChild(listItem);
 }
 
-// Function to add a superhero to favorites and update localStorage
-function addToFavorites(superhero) {
-    // Save the updated favorites list to localStorage
+// Function to remove a favorite superhero by ID
+function removeFavorite(superheroIdToRemove) {
     const favorites = loadFavoritesFromLocalStorage();
-    favorites.push(superhero);
-    localStorage.setItem("favoriteSuperheroes", JSON.stringify(favorites));
+    const updatedFavorites = favorites.filter(superhero => superhero.id != superheroIdToRemove);
+
+    localStorage.setItem("favoriteSuperheroes", JSON.stringify(updatedFavorites));
+    
 }
+
+
 
 // Function to clear favorite superheroes and update localStorage
 function clearFavorites() {
@@ -52,77 +65,3 @@ clearBtn.addEventListener('click', clearFavorites);
 
 // Load favorite superheroes and display them when the "favorites.html" page loads
 window.addEventListener("load", displayFavorites);
-
-var search_btn = document.getElementById("searchButton");
-search_btn.addEventListener('click', search_characters);
-
-function search_characters() {
-    var input = document.getElementById("search-input");
-    var userData = input.value;
-    const search_url = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${userData}&ts=1&apikey=fdeb3f85bfe23ee3a791b0f4c7ec18b9&hash=56a9ac5e3abbfa45fa88140cd53af50b`;
-
-    var searchResults = document.getElementById("search_list");
-
-    // Fetch data from the search_url
-    fetch(search_url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Network response was not ok: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(jsonData => {
-            // Clear previous search results
-            searchResults.innerHTML = "";
-
-            const bar = document.createElement("div");
-            bar.className = "bar";
-            bar.innerHTML = `<div><a href="./favorites.html">x</a></div>`;
-            searchResults.appendChild(bar);
-
-            // Display search results
-            const characters = jsonData.data.results;
-            for (let i = 0; i < characters.length; i++) {
-                const character = characters[i];
-                const searchItem = document.createElement("div");
-                searchItem.className = "search-item";
-                const imagePath = character.thumbnail.path.replace("http://", "https://");
-                const imageSrc = `${imagePath}.${character.thumbnail.extension}`;
-
-                searchItem.innerHTML = `
-                <div style="display: flex; align-items: center; flex-direction:row;">
-                
-                    <a href="../Characters/character.html?character=${character.id}">
-                    <img src="${imageSrc}" alt="${character.name}">
-                    </a><a href="../Characters/character.html?character=${character.id}">
-                    <p class="fs-3">${character.name}</p>
-                    </a>
-
-                    <i class="fa-regular fa-heart text-danger favoriteBtn fs-3 m-4 " style="cursor:pointer;"></i>
-                </div>
-                `;
-
-                // Add an event listener to the "Favorite" button
-                const favoriteBtn = searchItem.querySelector(".favoriteBtn");
-                favoriteBtn.addEventListener("click", function(event){
-                    addToFavorites(character);
-                    event.stopPropagation();
-                });
-
-                searchResults.appendChild(searchItem);
-            }
-
-            // Show the search results
-            searchResults.style.display = "flex";
-        })
-        .catch(error => {
-            console.error("Fetch error:", error);
-            searchResults.textContent = "An error occurred while fetching data.";
-        });
-}
-
-document.addEventListener('click',()=>{
-    var hide=document.getElementById("search_list");
-    hide.style.display="none";
-  
-  });
